d027a981ac9e58973e890e20ae6ed4880f73b1c1
Sourav <ksourav662@gmail.com>
2025-03-05 14:42:05 +0530

    Generics Constraints.

---
+++ Generics-Constraints.js
@@ -0,0 +1,12 @@
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    return Customer;
+}());
+function CustomerLogger(customer) {
+    console.log("FirstName - ".concat(customer.firstName, " \nLastname - ").concat(customer.lastName));
+}
+var customer = new Customer("Harry", "Potter");
+CustomerLogger(customer);
---
+++ Generics-Constraints.ts
@@ -0,0 +1,17 @@
+class Customer {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+function CustomerLogger<T extends Customer>(customer: T): void {
+  console.log(
+    `FirstName - ${customer.firstName} \nLastname - ${customer.lastName}`
+  );
+}
+
+const customer = new Customer("Harry", "Potter");
+CustomerLogger(customer);
